3d5873b95f74b0ebb4dd1c12d002963c87dfe9db
omer <omeryaldirgan@hotmail.com>
2025-01-05 00:52:54 +0300

    feat: implement useTypewriter hook and reducer for typewriter effect

---
+++ dist/cjs/reducer.js
@@ -0,0 +1,18 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.reducer = reducer;
+function reducer(state, action) {
+    var _a, _b;
+    switch (action.type) {
+        case "TYPE":
+            return Object.assign(Object.assign({}, state), { speed: action.speed, text: (_a = action.payload) === null || _a === void 0 ? void 0 : _a.substring(0, state.text.length + 1) });
+        case "DELAY":
+            return Object.assign(Object.assign({}, state), { speed: action.payload });
+        case "DELETE":
+            return Object.assign(Object.assign({}, state), { speed: action.speed, text: (_b = action.payload) === null || _b === void 0 ? void 0 : _b.substring(0, state.text.length - 1) });
+        case "COUNT":
+            return Object.assign(Object.assign({}, state), { count: state.count + 1 });
+        default:
+            return state;
+    }
+}
---
+++ dist/esm/reducer.d.ts
@@ -0,0 +1,21 @@
+type State = {
+    speed: number;
+    text: string;
+    count: number;
+};
+type Action = {
+    type: "DELAY";
+    payload: number;
+} | {
+    type: "TYPE";
+    payload: string;
+    speed: number;
+} | {
+    type: "DELETE";
+    payload: string;
+    speed: number;
+} | {
+    type: "COUNT";
+};
+export declare function reducer(state: State, action: Action): State;
+export {};
